3adcd4a901a3515f6498d3754421101019e6cbd5
castorga <castorga@inception.com>
2025-03-17 16:08:00 +0100

    mod wp-config.php

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -28,7 +28,6 @@
 // define( 'WP_HOME', getenv('https://login.42.fr') );
 // define( 'WP_SITEURL', getenv('https://login.42.fr') );
 
-
 define( 'DB_NAME', getenv('DB_NAME') );
 define( 'DB_USER', getenv('DB_USER') );
 define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
